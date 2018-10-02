261270416b44c127cac0622ecad6781a1e39db5d
maxim-iliano-s <maxim_iliano_s@mail.ru>
2018-10-02 22:03:32 +0300

    finished eith ContactData class

--- HomeWorkTestProject/HomeWorkTestProject/ContactData.cs
+++ HomeWorkTestProject/HomeWorkTestProject/ContactData.cs
@@ -12,25 +12,25 @@ namespace HW_WebAddressbookTests
         private string middlename;
         private string lastname;
         private string nickname;
-        private string photo;
+        //private string photo;
         private string title;
         private string company;
-        private string asddress;
+        private string address;
         // telephon;
         private string hometel;
         private string mobiletel;
         private string worktel;
-        private string fax;
+        //private string fax;
         private string e_mail;
-        private string e_mail2;
-        private string e_mail3;
+        //private string e_mail2;
+        //private string e_mail3;
         private string homepage;
         private string birthday;
         private string anniversary;
         private string group;
         // Secondary;
-        private string address;
-        private string home;
+        private string secaddress;
+        private string sechome;
         private string notes;
 
         public string Firstname
@@ -50,6 +50,95 @@ namespace HW_WebAddressbookTests
             }
             set => middlename = value;
         }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set => lastname = value;
+        }
+        public string Nickname
+        {
+            get
+            {
+                return nickname;
+            }
+            set => nickname = value;
+        }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set => title = value;
+        }
+        public string Company
+        {
+            get
+            {
+                return company;
+            }
+            set => company = value;
+        }
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set => address = value;
+        }
+        public string Hometel
+        {
+            get
+            {
+                return hometel;
+            }
+            set => hometel = value;
+        }
+        public string Mobiletel
+        {
+            get
+            {
+                return mobiletel;
+            }
+            set => mobiletel = value;
+        }
+        public string Worktel
+        {
+            get
+            {
+                return worktel;
+            }
+            set => worktel = value;
+        }
+        public string E_mail
+        {
+            get
+            {
+                return e_mail;
+            }
+            set => e_mail = value;
+        }
+
+        public string Homepage
+        {
+            get
+            {
+                return homepage;
+            }
+            set => homepage = value;
+        }
+       
+        public string Birthday { get => birthday; set => birthday = value; }
+        public string Anniversary { get => anniversary; set => anniversary = value; }
+        public string Group { get => group; set => group = value; }
+        public string Secaddress { get => secaddress; set => secaddress = value; }
+        public string Sechome { get => sechome; set => sechome = value; }
+        public string Notes { get => notes; set => notes = value; }
     }
 
 }
