02b2f537b88222a565ec4fbc56a187a1263c349b
maxim-iliano-s <maxim_iliano_s@mail.ru>
2018-10-03 00:56:11 +0300

    Everything  OK but Birthday-day is empty yet

---
+++ HomeWorkTestProject/HomeWorkTestProject/GroupData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_WebAddressbookTests
+{
+    class GroupData
+    {
+        private string gname;
+        private string gheader ="";
+        private string gfooter ="";
+
+        public GroupData(string gname)
+        {
+            this.gname = gname;
+        }
+
+        public string Gname { get => gname; set => gname = value; }
+        public string Gheader { get => gheader; set => gheader = value; }
+        public string Gfooter { get => gfooter; set => gfooter = value; }
+    }
+}
--- HomeWorkTestProject/HomeWorkTestProject/HW_GroupCreationWebAddressbookTest.cs
+++ HomeWorkTestProject/HomeWorkTestProject/HW_GroupCreationWebAddressbookTest.cs
@@ -45,27 +45,67 @@ namespace HW_WebAddressbookTests
         [Test]
         public void HW_WebAddressbookTest()
         {
-            driver.Navigate().GoToUrl(baseURL + "addressbook/");
-            driver.FindElement(By.Name("user")).Click();
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
-            driver.FindElement(By.Name("group_name")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("Home Work Group");
-            driver.FindElement(By.Name("group_header")).Click();
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("<There is <Group header (Logo):> should be here>");
-            driver.FindElement(By.Name("group_footer")).Click();
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("<Home Work Group>");
-            driver.FindElement(By.Name("submit")).Click();
+            OpenHomePage();
+            Login(new AccountData("admin","secret"));
+            OpenGroupPage();
+            InitGroupeCreation();
+            GroupData group = new GroupData("Focus Group A");
+            group.Gheader = "Focus A";
+            group.Gfooter = "Focus A footer";
+            FillOutGroupData(group);
+            SubmitGroupCreation();
+            ReturnToMainGroupPage();
+        }
+
+        private void ReturnToMainGroupPage()
+        {
             driver.FindElement(By.LinkText("group page")).Click();
         }
+
+        private void SubmitGroupCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillOutGroupData(GroupData group)
+        {
+            driver.FindElement(By.Name("group_name")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Gname);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Gheader);
+            driver.FindElement(By.Name("group_footer")).Click();
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Gfooter);
+        }
+
+        private void InitGroupeCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void OpenGroupPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "addressbook/");
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Click();
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+
         private bool IsElementPresent(By by)
         {
             try
